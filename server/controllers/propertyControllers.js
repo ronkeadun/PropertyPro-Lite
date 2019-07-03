eb54e650bb672db0b4749eb3e1aafd8e9c6f9dfa
ronkeadun <ronkeadun1@yahoo.com>
2019-07-04 00:55:55 +0100

    feature(endpoints): add features that create and update property advert [Delivers #167032876, #167032815]

--- server/controllers/propertyControllers.js
+++ server/controllers/propertyControllers.js
@@ -43,6 +43,76 @@ class Properties{
 		}
 	}
 
+	static createProAdvert(req, res){
+		const {error} = Properties.validateProperty(req.body)
+		if (error){
+		 	res.status(404).send({
+		 		status:"error",
+		 		message:error.details[0].message
+		 	})
+		 	return
+		}
+
+		const property = {
+			id: properties.length + 1,
+			owner: req.body.owner,
+			status:req.body.status,
+			price: req.body.price,
+			state: req.body.state,
+			city: req.body.city,
+			address:req.body.address,
+			type:req.body.type,
+			created_on:req.body.created_on,
+			image_url: ""
+		};
+		properties.push(property);
+		res.status(200).send({
+			status:"success",
+			data: property
+		})
+	}
+
+	static updateProAdvert(req, res){
+		//Look up the property
+		const property = properties.find((c)=>c.id === parseInt(req.params.propertyId));
+		
+		//If not existing return 404
+		if(!property){
+			res.status(404).send({
+		 		status:"error",
+		 		message:"The property with the given ID was not found"
+		 	})
+		 	return
+		}
+
+		//Validate
+		const {error} = Properties.validateProperty(req.body)
+		if (error){
+		 	res.status(404).send({
+		 		status:"error",
+		 		message:error.details[0].message
+		 	})
+		 	return
+		}
+
+		//Update property
+		property.owner = req.body.owner
+		property.status = req.body.status
+		property.price = req.body.price 
+		property.state = req.body.state
+		property.city = req.body.city
+		property.address = req.body.address
+		property.type = req.body.type
+		property.created_on = req.body.created_on
+		property.image_url = req.body.image_url
+		
+		//Return updated property
+		res.status(200).send({
+			status:"success",
+			data: property
+		})
+	}
+
 }
 
 export default Properties;
