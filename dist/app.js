33c14b393c5e1245ab700d50c1f0c4d2c59ac712
ronkeadun <ronkeadun1@yahoo.com>
2019-07-12 01:36:38 +0100

    feature: add feature to mark property as sold [Delivers #167032852]

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,45 @@
+'use strict';
 
-// # sourceMappingURL=app.js.map
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _propertyRouters = require('./server/routers/propertyRouters.js');
+
+var _propertyRouters2 = _interopRequireDefault(_propertyRouters);
+
+var _propertyControllers = require('./server/controllers/propertyControllers.js');
+
+var _propertyControllers2 = _interopRequireDefault(_propertyControllers);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// load express module/library(import express from node modules)
+var app = (0, _express2.default)(); // create an instance of express for use
+
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+
+app.use("/api/v1/properties", _propertyRouters2.default);
+
+var port = process.env.PORT || 3000;
+
+//start up application server
+app.listen(port, function () {
+	console.log('Server is up and listening on port ' + port);
+});
+
+exports.default = app;
+//# sourceMappingURL=app.js.map
--- server/routes/propertyRouters.js
+++ server/routes/propertyRouters.js
@@ -8,11 +8,17 @@ router.get("/", Properties.getAllProAdvert);
 //get a specific Property advert route
 router.get("/:propertyId", Properties.getSpecificProAdvert);
 
+//get a all Property advert of a specific type
+router.get("/:propertyId?type=propertyType", Properties.findProType);
+
 //create Property advert route
 router.post("/", Properties.createProAdvert);
 
 //update a Property route
 router.patch("/:propertyId", Properties.updateProAdvert);
+
+//mark a Property as sold
+router.patch("/:propertyId/sold", Properties.updateProStatus);
 
 //delete a Property route
 router.delete("/:propertyId", Properties.deleteSpecificRide);
